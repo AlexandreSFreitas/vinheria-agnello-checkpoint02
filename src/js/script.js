830acdc7e698b93180146de62f9b17c3b6d1ad3c
Alexandre Freitas Silva <alexandrefreitasx@outlook.com>
2025-05-15 18:43:16 -0300

    Adicionando a safra mais velha e a quantidade de todos estoques

--- src/js/script.js
+++ src/js/script.js
@@ -7,6 +7,9 @@ var todosNomes = "";
 var todosTipos = ""; 
 var todasSafras = ""; 
 var todasIdades = "";
+var todosEstoques = "";
+var idadeVelho = 9999999999999999;
+var nomeVelho = "";
 
 while (continuar == "Sim" || continuar == "sim") {
     cadastros++;
@@ -24,10 +27,10 @@ while (continuar == "Sim" || continuar == "sim") {
         var tipoDeVinho = prompt("Insira o tipo de vinho");
     }
     //Solicitando a safra
-    var safraAno = prompt("Qual é a safra do vinho?");
+    var safraAno = prompt("Qual é a ano da safra do vinho?");
     while(isNaN(safraAno) || safraAno == ''){
         alert("Por favor, insira o ano da safra!");
-        var safraAno = prompt("Qual é a safra do vinho?");
+        var safraAno = prompt("Qual é a ano da safra do vinho?");
     }
     //solicitando a quantidade do estoque
     var estoque = prompt("Digite a quantidade do estoque:");
@@ -36,8 +39,7 @@ while (continuar == "Sim" || continuar == "sim") {
         var estoque = prompt("Digite a quantidade do estoque:");
     }
     if (estoque < 5){
-    //ADICIONANDO A INFORMAÇÃO QUE O ESTOQUE ESTÁ BAIXO NO CONSOLE
-        console.log("ESTOQUE BAIXO");
+        console.log(`Vinho ${nome} está com estoque baixo`)
         estoqueBaixo++;
     }
     //ADICIONANDO IF PARA CASO A SAFRA SEJA DE 2020 PARA CIMA E CONDIÇÃO PARA SABER SE O VINHO É JOVEM
@@ -52,17 +54,22 @@ while (continuar == "Sim" || continuar == "sim") {
     else if (safraAno < 2015){
         var idade = 'Vinho Antigo';
     }
+    if (safraAno < idadeVelho){
+        idadeVelho = safraAno
+        nomeVelho = nome
+    }
 
     todosNomes += nome + ", ";
     todosTipos += tipoDeVinho + ", ";
     todasSafras += safraAno + ", ";
     todasIdades += idade + ", ";
 
-    var continuar = prompt("Você deseja cadastrar outro produto?");
+    var continuar = prompt("Você deseja cadastrar outro produto? Digite Sim para continuar, qualquer outro para parar.");
     if (continuar != "Sim" && continuar != "sim"){
         alert("Produtos cadastrados, por favor verifique as informações no console.")
         //MOSTRANDO AS INFORMAÇÕES NO CONSOLE
-        console.log(`-Nome: ${todosNomes}\n-Tipo: ${todosTipos}\n-Safra: ${todasSafras}\n-Idade: ${todasIdades}\n-Estoque: ${estoque}\n-Vinhos em estoque baixo: ${estoqueBaixo}\n-Cadastros: ${cadastros}`);
+        console.log(`-Nome: ${todosNomes}\n-Tipos: ${todosTipos}\n-Safras: ${todasSafras}\n-Idades: ${todasIdades}\n-Estoque: ${estoque}\n-Vinhos em estoque baixo: ${estoqueBaixo}\n-Cadastros: ${cadastros}/n-Estoques${todosEstoques}`);
+        console.log(`O vinho com a safra mais velha é ${nomeVelho} da safra ${idadeVelho}`);
     }
 }
 }
