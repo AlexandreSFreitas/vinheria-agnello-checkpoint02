ad971eae364c4f133affc184e469daff3b91ff31
João Vitor Parizotto Rocha <jvprocha.05@gmail.com>
2025-05-14 23:39:20 -0300

    Obrigando o usuário a colocar nome e tipo de vinho

--- src/js/script.js
+++ src/js/script.js
@@ -2,29 +2,18 @@
 function verificarEstoque(){
 
 //Solicitando o nome do vinho ao usuário. 
-var nomeDoVinho = prompt("Insira o nome do vinho.")
-//Caso o usuário não insira o nome do vinho, solicite novamente.
-if (nomeDoVinho == "") {
-    alert("O nome do vinho não foi inserido. Insira novamente.")
-    var nomeDoVinho = prompt("Insira o nome do vinho.")
+var nome = prompt("Insira o nome do vinho.");
+while(!isNaN(nome) || nome == ''){
+    alert("Por favor, digite um nome válido");
+    var nome = prompt("Insira o nome do vinho.");
 }
-//Cadastrando o nome do vinho no console.
-console.log("-Nome do vinho:", nomeDoVinho)
-
 //Solicitando o tipo de vinho ao usuário. 
-var tipoDeVinho = prompt("Insira o tipo de vinho: tinto, branco ou rosé")
+var tipoDeVinho = prompt("Insira o tipo de vinho");
 //Caso o usuário não insira o tipo de vinho, solicite novamente.
-if (tipoDeVinho == "") {
-    alert("O tipo de vinho não foi inserido. Insira novamente.")
-    var tipoDeVinho = prompt("Insira o tipo de vinho: tinto, branco ou rosé")
+while(!isNaN(tipoDeVinho) || tipoDeVinho == ''){
+    alert("Por favor, digite um tipo de vinho!");
+    var tipoDeVinho = prompt("Insira o tipo de vinho");
 }
-//Caso o usuário não insira um dos três tipos de vinho, solicite novamente.
-else if (tipoDeVinho != "tinto" && tipoDeVinho != "branco" && tipoDeVinho != "rosé" && tipoDeVinho != "Tinto" && tipoDeVinho != "Branco" && tipoDeVinho != "Rosé" ){
-    alert("Tipo não encontrado. Insira um tipo válido.")
-    var tipoDeVinho = prompt("Insira o tipo de vinho: tinto, branco ou rosé")
-//Cadastrando o tipo de vinho no console.
-}
-console.log("-Tipo de vinho:", tipoDeVinho)
 
 //ADICIONANDO A VAR. DA SAFRA
 var safraAno = prompt("Qual a safra do vinho?");
